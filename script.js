0a8bbf0e8c7235da6249b2fbd917c3add5ca4265
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-02 22:19:35 +0000

    add utils, game engine and players consts

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const gameSettings = {
-  // Suits
-  suits: ["♣", "♦", "♥", "♠"],
-  // Face of cards
-  ranks: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
-  // Number of decks
-  numberOfDecks: 8,
+const utils = {
+  suits: ["♣", "♦", "♥", "♠"], // Suits
+  ranks: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], // Face of cards
+  maxNumberOfDecks: 8, // Maximum number of decks, default is 1
+  maxNumberOfPlayers: 7, // Maximum number of players, default is 1
 };
 
+const engine = {
+  numberOfPlayers: setPlayers(players[0]), // default is 1 player
+};
+
+const players = [
+  { name: "Player 1", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 2", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 3", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 4", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 5", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 6", hands: [], score: { wins: 0, losses: 0 } },
+  { name: "Player 7", hands: [], score: { wins: 0, losses: 0 } },
+];
+
+// set how many players will be on table
+function setPlayers() {
+  // TODO finish the method
+}
+
 // Generate a deck of cards
-function generateDeck(suits, ranks, numberOfDecks) {
+function generateDeck(suits, ranks, numberOfDecks = 1) {
   var deck = [];
   for (var i = 0; i < numberOfDecks; i++) {
     for (var suit of suits) {
