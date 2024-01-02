90f1f7d0720f816cd7145cbb579626e67beb3ad8
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-02 18:11:18 +0000

    add shuffleDeck function

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const gameSettings = {
   // Suits
   suits: ["♣", "♦", "♥", "♠"],
-  // face of cards
+  // Face of cards
   ranks: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
-  // MAX numbers of decks that can be add
-  maxDecks: 8,
-  // MIN numbers of decks that can be add (default)
-  minDecks: 1,
+  // Number of decks
+  numberOfDecks: 8,
 };
 
 // Generate a deck of cards
 function generateDeck(suits, ranks, numberOfDecks) {
-  let deck = [];
+  var deck = [];
 
-  for (let i = 0; i < numberOfDecks; i++) {
-    for (let suit of suits) {
-      for (let rank of ranks) {
+  for (var i = 0; i < numberOfDecks; i++) {
+    for (var suit of suits) {
+      for (var rank of ranks) {
         deck.push(`${rank}${suit}`);
       }
     }
@@ -25,5 +23,22 @@ function generateDeck(suits, ranks, numberOfDecks) {
 }
 
 // Generate deck with all card
-var deck = generateDeck(suits, ranks, maxDecks);
-console.log("🚀 ~ file: script.js:41 ~ deck:", deck);
+var deck = generateDeck(
+  gameSettings.suits,
+  gameSettings.ranks,
+  gameSettings.numberOfDecks
+);
+
+function shuffleDeck(deck) {
+  var shuffledDeck = [...deck];
+
+  for (var i = shuffledDeck.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+  }
+
+  return shuffledDeck;
+}
+
+var shuffledDeck = shuffleDeck(deck);
+console.log("🚀 ~ file: script.js:45 ~ shuffledDeck:", shuffledDeck);
